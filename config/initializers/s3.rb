799c7cde309833e1796c69eefe50a2b398d669d8
Auriga <area51alienware@gmail.com>
2017-05-07 19:28:30 +0000

    Added env var

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAI243RJZGRU43JK4Q",
-      :aws_secret_access_key  => "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",
+      :aws_access_key_id      => ENV["KEY_ID"],
+      :aws_secret_access_key  => ENV["SECRET_ACCESS"],
       :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "techauriga"
