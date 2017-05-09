0eab676bf92a0fbbec8a703dfaf9bb00fb22d14e
Auriga <area51alienware@gmail.com>
2017-05-09 02:13:05 +0000

    Testing

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -19,20 +19,20 @@ class AdsController < ApplicationController
       IO.copy_stream(open(s3_path), "public/uploads/#{@ad[:id]}/#{@ad[:image]}")
       image_path = "public/uploads/#{@ad[:id]}/#{@ad[:image]}"
       begin
-        classify = `python db/classify_image.py --image_file #{image_path}`
+        #classify = `python db/classify_image.py --image_file #{image_path}`
+        classify = `python db/algorithm/train #{@ad[:id]}  #{image_path}`
       rescue
         classify = "Image must be a jpg for image recognition to work. Stay tuned!"
       end
       result = `python db/feedback_alg.py  #{image_path}`
       ad_rating = `python db/rating_alg.py #{image_path}`
-      ml_results = `python db/algorithm/train #{@ad[:id]}  #{image_path}`
       if classify.length >= 200
           print(classify.length)
           print(classify)
           temp = classif.index('bytes.')
           classify = classify[temp+6..-1]
       end
-      @ad.update_attributes(:feedback => ml_results, :rating => ad_rating, :recon => classify)
+      @ad.update_attributes(:feedback => result, :rating => ad_rating, :recon => classify)
       redirect_to :action => :index
       #redirect_to ads_path(@ad)
   end
