1413e345aaf6d7379b924dcfe9e25c5b331086fb
Auriga <area51alienware@gmail.com>
2017-05-09 08:26:47 +0000

    Gitignore did not work. Had to update keys

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAIEIF6ASH6Z6LEOKQ',  
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      =>  ENV["KEY_ID"],  
+      :aws_secret_access_key  =>  ENV["SECRET_ACCESS"],
       :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "techauriga"
