e92ac1750e50cdadd82d48760c4c37e6e7b6fd60
AG-Systems <area51alienware@gmail.com>
2017-09-26 06:38:44 +0000

    Small change

--- db/delete_models.py
+++ db/delete_models.py
@@ -1,10 +1,19 @@
 import os
+from os.path import join
+import sys
 
 os.chdir('..')
-os.remove("train_data.npy")
 os.remove("checkpoint")
+files = os.listdir(os.curdir) 
+for item in files:
+    if item.endswith(".npy") or item.endswith(".csv") or item.endswith(".index") or item.endswith(".meta"):
+        os.remove(item)
+"""
+os.remove("train_data.npy")
+
 os.remove("posvsneg-0.001-6conv-basic.model.data-00000-of-00001")
 os.remove("posvsneg-0.001-6conv-basic.model.index")
 os.remove("posvsneg-0.001-6conv-basic.model.meta")
 os.remove("submission_file.csv")
+"""
 print("SUCCESS")
